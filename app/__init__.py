cdfa31167329eb9f9bbb61bd52f8c823c1fe9f22
David Adamo Jr <me@davidadamojr.com>
2020-04-21 21:01:04 -0400

    add migration files

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 from config import Config
 
 
@@ -8,5 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
