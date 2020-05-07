c51e8e29acef1161e70c734c00b00a300d8770f5
David Adamo Jr <me@davidadamojr.com>
2020-05-07 18:56:47 -0400

    add env file

--- microblog.py
+++ microblog.py
@@ -1,12 +1,10 @@
-from app import app, db
+from app import create_app, db, cli
 from app.models import User, Post
-from app import cli
+
+app = create_app()
+cli.register(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
